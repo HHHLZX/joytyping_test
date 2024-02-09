b9c968b95922382c9b286c87713ec82efb3856da
HHHLZX <138979515+HHHLZX@users.noreply.github.com>
2024-02-09 15:05:59 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module hhhlzx.xbox.joytyping.joytyping_test {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens hhhlzx.xbox.joytyping.joytyping_test to javafx.fxml;
+    exports hhhlzx.xbox.joytyping.joytyping_test;
+}
